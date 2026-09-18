cab72806d6bf37b780ab31d2ab91c2e676ec4552
tompol1123581321 <tom.polivka96@gmail.com>
2026-09-18 23:49:50 +0200

    descriptions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://hlavacekautoskola.cz",
   integrations: [react(), icon()],
   vite: {
     plugins: [tailwindcss()],
